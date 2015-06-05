a397e84c7b5d6621d1717c4015c3866c419f352a
J. Gavin Ray <jgavinray@gmail.com>
2015-06-04 19:52:57 -0700

    removed unused $app variable

--- src/controllers.php
+++ src/controllers.php
@@ -5,13 +5,13 @@ use DaGopherboy\SilexJWTRestPhp\Routes\Open\RootRouteProvider;
 
 $app->mount('/', new RootRouteProvider);
 
-$app->error(function (\Exception $e, $code) use ($app) {
+$app->error(function (\Exception $e, $code) {
     switch ($code) {
         case 404:
             $message = 'The requested page could not be found.';
             break;
         default:
-            $message = 'We are sorry, but something went terribly wrong.';
+            $message = "We are sorry, but something went terribly wrong. $e";
     }
     return new Response($message, $code);
 });
